1d660fe24dd6c543b34e70ac3a0ed2b8cb944728
Rahul Joshi <rahul@fieldwire.com>
2021-04-18 23:25:19 -0700

    Added first networking API

--- Sources/RJCore/Networking.swift
+++ Sources/RJCore/Networking.swift
@@ -9,6 +9,25 @@ import Foundation
 
 extension RJCore {
     public class Networking {
+        /// Responsible for handling all networking calls
+        /// - Warning: Must create befor using any public APIs
+        public class Manager {
+            public init() { }
+            
+            private let urlSession = URLSession.shared
+            
+            public func loadData(from url: URL, completionHandler: @escaping (NetworkResult<Data>) -> Void) {
+                let task = urlSession.dataTask(with: url) { (data, response, error) in
+                    let result = data.map(NetworkResult<Data>.success) ?? .failure(error)
+                    completionHandler(result)
+                }
+                task.resume()
+            }
+        }
         
+        public enum NetworkResult<Value> {
+            case success(Value)
+            case failure(Error?)
+        }
     }
 }
--- Tests/RJCoreTests/RJCoreNetworkingTests.swift
+++ Tests/RJCoreTests/RJCoreNetworkingTests.swift
@@ -6,15 +6,31 @@
 //
 
 import XCTest
+@testable import RJCore
 
 final class RJCoreNetworkingTests: XCTestCase {
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func loadDataCall() {
+        let manager = RJCore.Networking.Manager()
+        let expectation = XCTestExpectation(description: "Called for Data")
+        
+        guard let url = URL(string: "https://google.com") else {
+            return XCTFail("Could not create URL")
+        }
+        
+        manager.loadData(from: url) { result in
+            expectation.fulfill()
+            switch result {
+            case .success(let returnedData):
+                XCTAssertNotNil(returnedData)
+            case .failure(let error):
+                XCTFail(error?.localizedDescription ?? "Error forming error")
+            }
+        }
+        
+        wait(for: [expectation], timeout: 5)
     }
-    
     static var allTests =  [
-        ("testExample", testExample)
+        ("loadDataCall", loadDataCall)
     ]
 }
